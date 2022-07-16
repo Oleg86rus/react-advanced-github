7de7c9c4e471c8e0c074d639546610c58a356c35
oleg086rus <o.pototskiy@yandex.ru>
2022-07-16 15:02:04 +0500

    some fix.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
           <Navigation/>
           <Routes>
             <Route path='/' element={<HomePage/>}/>
-            <Route path='/favourite' element={<FavouritePage/>}/>
+            <Route path='/favourites' element={<FavouritePage/>}/>
           </Routes>
       </>
   );
